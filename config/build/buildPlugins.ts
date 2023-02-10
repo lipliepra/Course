70cf5e023ca695c663fc4beaa9489a1f26e4162a
lipliepra <dzis99@yandex.ru>
2023-02-10 19:39:30 +0500

    Add scss

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,13 +1,18 @@
 import webpack from "webpack";
 import HTMLWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 import {IBuildOptions} from "./types/config";
 
-export function buildPlugins({paths}: IBuildOptions): webpack.WebpackPluginInstance[] {
+export function buildPlugins(options: IBuildOptions): webpack.WebpackPluginInstance[] {
     return [
         new HTMLWebpackPlugin({
-            template: paths.html,
+            template: options.paths.html,
         }),
         new webpack.ProgressPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].[contenthash:8].css',
+            chunkFilename: 'css/[name].[contenthash:8].css',
+        })
     ];
 }
--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,8 +1,10 @@
 import webpack from "webpack";
+
 import {buildPlugins} from "./buildPlugins";
 import {buildLoaders} from "./buildLoaders";
 import {buildResolvers} from "./buildResolvers";
 import {buildDevServer} from "./buildDevServer";
+
 import {IBuildOptions} from "./types/config";
 
 export function buildWebpackConfig(options: IBuildOptions): webpack.Configuration {
@@ -18,10 +20,14 @@ export function buildWebpackConfig(options: IBuildOptions): webpack.Configuratio
         },
         plugins: buildPlugins(options),
         module: {
-            rules: buildLoaders(),
+            rules: buildLoaders(options),
         },
         resolve: buildResolvers(),
-        devtool: isDevelopment ? 'inline-source-map' : undefined,
-        devServer: isDevelopment ? buildDevServer(options) : undefined,
+        devtool: isDevelopment
+            ? 'inline-source-map'
+            : undefined,
+        devServer: isDevelopment
+            ? buildDevServer(options)
+            : undefined,
     }
 }
