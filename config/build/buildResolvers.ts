9c3893fecfefbeadb02e2ec005c71f8486ffeafc
lipliepra <dzis99@yandex.ru>
2023-02-11 14:26:34 +0500

    Add absolute paths

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -1,5 +1,12 @@
-export function buildResolvers(): { extensions: string[] } {
+import {ResolveOptions} from "webpack";
+import {IBuildOptions} from "./types/config";
+
+export function buildResolvers(options: IBuildOptions): ResolveOptions {
     return {
         extensions: ['.tsx', '.ts', '.js'],
+        preferAbsolute: true,
+        modules: [options.paths.src, 'node_modules'],
+        mainFiles: ['index'],
+        alias: {}
     }
 }
