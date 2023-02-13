9035e43f8fd9e1dbe23df9bfe4000b4712dbd25a
lipliepra <dzis99@yandex.ru>
2023-02-13 18:33:08 +0500

    Fix

--- src/widgets/Sidebar/ui/SideBar/SideBar.tsx
+++ src/widgets/Sidebar/ui/SideBar/SideBar.tsx
@@ -3,6 +3,7 @@ import React, {useState} from 'react';
 import {classNames} from "shared/lib/classNames";
 import cls from './SideBar.module.scss';
 import {ThemeSwitcher} from "widgets/ThemeSwitcher";
+import {LangSwitcher} from "widgets/LangSwitcher/LangSwitcher";
 
 interface ISideBarProps {
     className?: string;
@@ -21,7 +22,7 @@ export const SideBar: React.FC<ISideBarProps> = ({className}) => {
             <button onClick={onToggle}>toggle</button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
-                {/*<LangSwitcher />*/}
+                <LangSwitcher className={cls.lang} />
             </div>
         </div>
     );
